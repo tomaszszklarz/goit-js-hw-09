43d3449f8dc41c5f8319c584f9f9d3a639ed38bb
Tomas Szklarz <tomaszszklarz@icloud.com>
2022-09-01 13:25:31 +0200

    update task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,17 +7,19 @@ const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 let colorMix = null;
 
-startButton.addEventListener('click', () => {
+const colorStart = () => {
     startButton.disabled = true;
     stopButton.disabled = false;
     colorMix = setInterval(() => {
         const color = getRandomHexColor();
         body.style.background = color;
     }, 1000)
-});
-
-stopButton.addEventListener('click', () => {
+};
+const colorStop = () => {
     stopButton.disabled = true;
     startButton.disabled = false;
     clearInterval(colorMix);
-})
+};
+
+startButton.addEventListener('click', colorStart);
+stopButton.addEventListener('click', colorStop);
