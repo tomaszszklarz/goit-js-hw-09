23e6e1927a34ccf7cd8200759b8f6d25d880e466
Tomas Szklarz <tomaszszklarz@icloud.com>
2022-09-01 13:20:12 +0200

    task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let colorMix = null;
+
+startButton.addEventListener('click', () => {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    colorMix = setInterval(() => {
+        const color = getRandomHexColor();
+        body.style.background = color;
+    }, 1000)
+});
+
+stopButton.addEventListener('click', () => {
+    stopButton.disabled = true;
+    startButton.disabled = false;
+    clearInterval(colorMix);
+})
