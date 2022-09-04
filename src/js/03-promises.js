690ae2bd6ce10cde7a47649ffc2d1df1eaf49568
Tomas Szklarz <tomaszszklarz@icloud.com>
2022-09-04 10:51:29 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const labels = Array.from(document.querySelectorAll('label'));
+const inputs = Array.from(document.querySelectorAll('input'));
+const form = document.querySelector('.form');
+const submitBtn = document.querySelector(`button[type="submit"]`);
+
+const createPromise = (position, delay) => {
+  return new Promise((resolve, reject) => {
+    let setTime = setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const valueAmount = Number(form.elements.amount.value);
+  const valueStep = Number(form.elements.step.value);
+  const valueDelay = Number(form.elements.delay.value);
+
+  for (let i = 1; i <= valueAmount; i += 1) {
+    let stepTime = valueDelay + valueStep * (i - 1);
+    createPromise(i, stepTime)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay} ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay} ms`
+        );
+      });
   }
-}
+});
